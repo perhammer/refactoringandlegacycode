2ba758f0f7da7a076d84be19c972f13ac09fb127
perhammer <github@perhammer.com>
2017-11-07 20:37:37 +0000

    Added sulfuras tests

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -76,4 +76,34 @@ public class GildedRoseTest {
         assertEquals(-2, app.items[0].sellIn);
         assertEquals(50, app.items[0].quality);
     }
+
+    @Test
+    public void testSulfurasBeforeSellDate() {
+        Item[] items = new Item[] { new Item("Sulfuras, Hand of Ragnaros", 5, 5) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals("Sulfuras, Hand of Ragnaros", app.items[0].name);
+        assertEquals(5, app.items[0].sellIn);
+        assertEquals(5, app.items[0].quality);
+    }
+
+    @Test
+    public void testSulfurasOnSellDate() {
+        Item[] items = new Item[] { new Item("Sulfuras, Hand of Ragnaros", 0, 5) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals("Sulfuras, Hand of Ragnaros", app.items[0].name);
+        assertEquals(0, app.items[0].sellIn);
+        assertEquals(5, app.items[0].quality);
+    }
+
+    @Test
+    public void testSulfurasAfterSellDate() {
+        Item[] items = new Item[] { new Item("Sulfuras, Hand of Ragnaros", -1, 5) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals("Sulfuras, Hand of Ragnaros", app.items[0].name);
+        assertEquals(-1, app.items[0].sellIn);
+        assertEquals(5, app.items[0].quality);
+    }
 }
